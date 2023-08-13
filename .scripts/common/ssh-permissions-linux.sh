fed5ce33850ec9a07a414f4d9902dc21641790c7
Igor Leonovich <mail@domain.com>
2023-08-14 02:02:57 +0400

    permissions-ssh: Add root

--- .scripts/common/ssh-permissions-linux.sh
+++ .scripts/common/ssh-permissions-linux.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-SSH_PATH=$HOME/.ssh
-chmod 700 $SSH_PATH
-for file in "`find $SSH_PATH -type f | grep -vP "\.pub$"`"; do chmod 600 $file; done
-for file in "`find $SSH_PATH -type f | grep -P "\.pub$"`"; do chmod 644 $file; done
+USER_SSH_PATH=$HOME/.ssh
+ROOT_SSH_PATH=/root
+SSH_PATHS=( $USER_SSH_PATH $ROOT_SSH_PATH )
+for SSH_PATH in "${SSH_PATHS[@]}"
+do
+  chmod 700 $SSH_PATH
+  for file in "`find $SSH_PATH -type f | grep -vP "\.pub$"`"; do chmod 600 $file; done
+  for file in "`find $SSH_PATH -type f | grep -P "\.pub$"`"; do chmod 644 $file; done
+done
